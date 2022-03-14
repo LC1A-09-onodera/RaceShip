c0f8e903b2ab45de4e0b7b2f7aa23996067ac8b0
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-03-14 17:11:53 +0900

    爆弾衝突判定作成途中

--- Bomb/Bomb.cpp
+++ Bomb/Bomb.cpp
@@ -1,5 +1,6 @@
 #include "Bomb.h"
 #include "../DX12operator.h"
+#include "../Shader/ShaderManager.h"
 Bomb::Bomb()
 {
 }
@@ -10,10 +11,22 @@ Bomb::~Bomb()
 
 void Bomb::Init()
 {
+	bombObject.CreateModel("Block", ShaderManager::playerShader);
+	blastObject.CreateModel("Block", ShaderManager::playerShader);
 }
 
 void Bomb::Update()
 {
+	if (data.isAlive)
+	{
+		BombUpdate();
+	}
+	if (data.isBlast)
+	{
+		BlastUpdate();
+	}
+	bombObject.position = data.pos;
+	blastObject.position = data.pos;
 }
 
 void Bomb::Finailize()
@@ -34,4 +47,32 @@ bool Bomb::Shot(DirectX::XMFLOAT3 angle, DirectX::XMFLOAT3 pos)
 	data.isAlive = true;
 	data.bombAngle = ConvertXMFLOAT3toXMVECTOR(angle);
 	return true;
+}
+
+XMFLOAT3 Bomb::EnemyBombCollision(EnemyBase enemyData)
+{
+	//“G‚Æ“–‚½‚Á‚Ä‚¢‚é‚©‚Ì”»’è
+	float bombEnemyDistance = 
+	//”š’e‚©‚ç“G‚ÉŒü‚©‚Á‚Ä‚Ì•ûŒüŒvZ
+
+	//“G‚Æ”š’e‚Æ‚Ì‹——£ŒvZ
+
+	//”š”­‚Ì”ÍˆÍ‚Æ”š•—‚Ì”ÍˆÍ‚Ì’·‚³‚ğ”äŠr
+
+	//”ä—¦‚É‰‚¶‚Ä‰Ÿ‚µ–ß‚µ‚Ì—Í‚Ì‘å‚«‚³‚ğ•ÏX
+
+	//Œü‚«‚Æ—Í‚Ì‘å‚«‚³‚ğæZ‚µ‚ÄƒxƒNƒgƒ‹‚É‚µ‚ÄŠµ«
+	DirectX::XMVECTOR blastVector = {};
+
+	return XMFLOAT3();
+}
+
+void Bomb::BombUpdate()
+{
+	DirectX::XMVECTOR moveSpeed = (data.bombAngle * data.bombSpeed);
+	data.pos += moveSpeed;
+}
+
+void Bomb::BlastUpdate()
+{
 }
--- Bomb/Bomb.h
+++ Bomb/Bomb.h
@@ -1,25 +1,31 @@
 #pragma once
 #include <DirectXMath.h>
-
-
+#include "../sampleObject/sampleObject.h"
+#include "../Enemy/Enemy.h"
 struct BombData
 {
 	DirectX::XMVECTOR pos = {};//爆弾の座標
 	DirectX::XMVECTOR bombAngle = {0, 0, 0};
-	float bombRadius = 0;//爆弾自体の大きさ
+	float bombRadius = 0.0f;//爆弾自体の大きさ
+	float bombSpeed = 0.0f;//爆弾の速度
 	float blastRadius = 10;//爆発の半径
 	float blastPower = 10;//爆風の強さ
 	int blastTimer = 30;//爆発タイマー(とりあえず使わない)
-	bool isAlive = false;
+	bool isAlive = false;//爆弾が現在生きているかどうか
+	bool isBlast = false;//爆風が発生しているかどうか
 };
 class Bomb
 {
 public:
 	Bomb();
 	~Bomb();
+	//初期化
 	void Init();
+	//更新処理
 	void Update();
+	//終了処理
 	void Finailize();
+	//描画処理
 	void Draw();
 
 	/// <summary>
@@ -29,6 +35,15 @@ public:
 	/// <param name="pos"></param>
 	/// <returns></returns>
 	bool Shot(DirectX::XMFLOAT3 angle, DirectX::XMFLOAT3 pos);
+
+	XMFLOAT3 EnemyBombCollision(EnemyBase enemyData);
+private:
+	//爆弾が生きている際の更新処理
+	void BombUpdate();
+	//爆発しているときの更新処理
+	void BlastUpdate();
 private:
 	BombData data;
+	SampleObject bombObject;
+	SampleObject blastObject;
 };
