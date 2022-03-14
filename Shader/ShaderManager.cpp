a621d94ca8fc0e537869df1b09a1e292e6738059
shlomon53 <yuusuke6t@outlook.jp>
2022-03-14 15:34:11 +0900

    shaderManager

---
+++ Shader/ShaderManager.cpp
@@ -0,0 +1,7 @@
+#include"ShaderManager.h"
+
+void ShaderManager::LoadShaders()
+{
+	playerShader.vsBlob = LoadShader(L"Resource/HLSL/OBJVertexShader.hlsl", "vs_5_0");
+	playerShader.psBlob = LoadShader(L"Resource/HLSL/OBJPixelShader.hlsl", "ps_5_0");
+}
