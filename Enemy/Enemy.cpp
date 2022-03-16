b027f7d421585b75aa9a035c9ccb04fed55583a5
shlomon53 <yuusuke6t@outlook.jp>
2022-03-16 10:25:32 +0900

    enemyの機能拡張

--- Enemy/Enemy.cpp
+++ Enemy/Enemy.cpp
@@ -3,3 +3,69 @@
 #include "../Particle/Particle3D.h"
 #include "../Sound/Sound.h"
 #include "../King/King.h"
+#include "../WindowsAPI/WinAPI.h"
+
+void EnemyBase::Init()
+{
+
+	SetAlive();
+}
+void EnemyBase::Init(XMFLOAT3 position, EnemyType enemyType, int hp, float moveSpeed)
+{
+	if (enemyType == EnemyType::NONE)
+	{
+
+		type = enemyType;
+	}
+	SetAlive();
+}
+void EnemyBase::Update()
+{
+
+}
+void EnemyBase::Draw()
+{
+	enemy.Update();
+	Draw3DObject(enemy);
+}
+void EnemyBase::SetAlive(int hp, float moveSpeed)
+{
+	SetRandomPosition();
+	this->hp = hp;
+	this->moveSpeed = moveSpeed;
+	isDead = false;
+	isWind = false;
+	kingDirection = { 1, 0, 0 };
+	windDirection = { 0, 0, 0 };
+}
+void EnemyBase::SetRandomPosition()
+{
+	//左右から出てくる
+	if (rand() % 2 == 0)
+	{
+		if (rand() % 2 == 0)
+		{
+			enemy.position.m128_f32[0] = WindowsAPI::window_width + 10;
+		}
+		else
+		{
+			enemy.position.m128_f32[0] = -WindowsAPI::window_width - 10;
+		}
+		//上下の調整
+		enemy.position.m128_f32[2] = (rand() % WindowsAPI::window_height) - (WindowsAPI::window_height / 2);
+	}
+	//上下から出てくる
+	else
+	{
+		if (rand() % 2 == 0)
+		{
+			enemy.position.m128_f32[2] = WindowsAPI::window_height + 10;
+		}
+		else
+		{
+			enemy.position.m128_f32[2] = -WindowsAPI::window_height - 10;
+		}
+		//左右の調整
+		enemy.position.m128_f32[0] = (rand() % WindowsAPI::window_width) - (WindowsAPI::window_width / 2);
+	}
+}
