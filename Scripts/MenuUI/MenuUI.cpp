6d326f7b0f9448282e03a1f5fb184426b6a806cc
yusuke onodera <yuusuke6t@outlook.jp>
2022-07-13 23:22:48 +0900

    表示UIの変更

--- Scripts/MenuUI/MenuUI.cpp
+++ Scripts/MenuUI/MenuUI.cpp
@@ -18,23 +18,40 @@ void MenuUI::Init(float height)
 void MenuUI::DrawUI(BaseDirectX& baseDirectX)
 {
 	int i = 0;
+	int j = 0;
 	const float uiSize = 32.0f;
-	for (auto itr = Rewired::KeyCodeString::keyboardKeys.begin(); itr != Rewired::KeyCodeString::keyboardKeys.end(); ++itr)
+	for (auto itr = Rewired::RewiredContainer::rewiredsC.begin(); itr != Rewired::RewiredContainer::rewiredsC.end(); ++itr)
 	{
-		Sprite sprite;
-		sprite = std::get<2>(*itr);
-		sprite.position.m128_f32[0] = i * uiSize;
-		sprite.SpriteDraw(baseDirectX);
-		i++;
-	}
-	i = 0;
-	for (auto itr = Rewired::KeyCodeString::padKeys.begin(); itr != Rewired::KeyCodeString::padKeys.end(); ++itr)
-	{
-		Sprite sprite;
-		sprite = std::get<2>(*itr);
-		sprite.position.m128_f32[0] = i * uiSize;
-		sprite.position.m128_f32[1] = uiSize;
-		sprite.SpriteDraw(baseDirectX);
-		i++;
+		for (auto keyItr = Rewired::KeyCodeString::keyboardKeys.begin(); keyItr != Rewired::KeyCodeString::keyboardKeys.end(); ++keyItr)
+		{
+			for (auto keysItr = itr->keys.begin(); keysItr != itr->keys.end(); ++keysItr)
+			{
+				if (std::get<1>(*keyItr) == *keysItr)
+				{
+					Sprite sp = std::get<2>(*keyItr);
+					sp.position.m128_f32[0] = i * uiSize;
+					sp.position.m128_f32[1] = j * uiSize;
+					sp.SpriteDraw(baseDirectX);
+					i++;
+				}
+			}
+			
+		}
+		for (auto padItr = Rewired::KeyCodeString::padKeys.begin(); padItr != Rewired::KeyCodeString::padKeys.end(); ++padItr)
+		{
+			for (auto padsItr = itr->padKeys.begin(); padsItr != itr->padKeys.end(); ++padsItr)
+			{
+				if (std::get<1>(*padItr) == *padsItr)
+				{
+					Sprite sp = std::get<2>(*padItr);
+					sp.position.m128_f32[0] = i * uiSize;
+					sp.position.m128_f32[1] = j * uiSize;
+					sp.SpriteDraw(baseDirectX);
+					i++;
+				}
+			}
+		}
+		i = 0;
+		j++;
 	}
 }
